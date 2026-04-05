c9bc1fee35ceaccdf0dfd0769d03c36ebccceab0
Victor Jacob <victorbidaultjacob@gmail.com>
2026-04-05 19:43:36 +0200

    Add : Ajout des moyennes pré-match

--- premier_league.py
+++ premier_league.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 premier_league_df = pd.read_csv("premier-league-matches.csv")
 
@@ -24,8 +25,19 @@ premier_league_df["HomeGoals"].hist(bins=10)
 plt.title('distribution but à domicile')
 plt.show()
 
-print()
+
 premier_league_df["Date"] = pd.to_datetime(premier_league_df["Date"])
 
+premier_league_df["home_goals_avg"] = premier_league_df.groupby("Home")["HomeGoals"].transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean())
+premier_league_df["away_goals_avg"] = premier_league_df.groupby("Away")["AwayGoals"].transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean())
 
-premier_league_df["home_goals_avg"] = premier_league_df.groupby("Home")["HomeGoals"].transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean()
+premier_league_df["home_conceded_avg"] = premier_league_df.groupby("Home")["AwayGoals"].transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean())
+premier_league_df["away_conceded_avg"] = premier_league_df.groupby("Away")["HomeGoals"].transform(lambda x: x.shift(1).rolling(5, min_periods=1).mean())
+
+premier_league_df = premier_league_df.dropna()
+
+premier_league_df["home_encoded"] = le_temp.fit_transform(premier_league_df["Home"])
+premier_league_df["away_encoded"] = le_temp.fit_transform(premier_league_df["Away"])
+premier_league_df["results_encoded"] = le_temp.fit_transform(premier_league_df["results"])
+
+print(premier_league_df)
